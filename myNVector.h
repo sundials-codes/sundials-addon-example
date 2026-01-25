f242845795075ad0e7a730762d980e8fbc9c7875
David J. Gardner <gardner48@llnl.gov>
2026-01-25 09:55:42 -0800

    update copyright 2026

--- myNVector.h
+++ myNVector.h
@@ -2,7 +2,7 @@
  * Programmer(s): Daniel R. Reynolds @ UMBC
  * -----------------------------------------------------------------
  * SUNDIALS Copyright Start
- * Copyright (c) 2025, Lawrence Livermore National Security,
+ * Copyright (c) 2025-2026, Lawrence Livermore National Security,
  * University of Maryland Baltimore County, and the SUNDIALS contributors.
  * Copyright (c) 2013-2025, Lawrence Livermore National Security
  * and Southern Methodist University.
